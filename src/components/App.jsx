c907b5f998e46a8ab432395d3b4377b79d810cc1
Andrii Hutnyk <andreigutnik@gmail.com>
2023-08-17 07:57:30 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,11 @@
-import {Profile} from './Profile/Profile';
+import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import user from './dataJson/user.json';
-import data from './dataJson/data.json';
-import friends from './dataJson/friends.json';
-import transactions from './dataJson/transactions.json';
-
+import user from '../dataJson/user.json';
+import data from '../dataJson/data.json';
+import friends from '../dataJson/friends.json';
+import transactions from '../dataJson/transactions.json';
 
 export const App = () => {
   return (
@@ -19,8 +18,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-			<FriendList friends={friends} />
-			<TransactionHistory items={transactions} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
